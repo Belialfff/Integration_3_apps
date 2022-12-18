e360f042ebfb776d324c9e22c09a3184d098f514
Belialfff <Horrorcatlsh@mail.ru>
2022-12-18 23:33:09 +0300

    Добавил в комментарии шаблон с типами данных для put и update метдов

--- customers_routes.py
+++ customers_routes.py
@@ -1,44 +1,62 @@
+"""Belialfff"""
+"""4 типа запросов(get,put,patch,delete) для таблицы customer"""
 
-from app import app, jsonify, db, request, ma
+
+from flask import jsonify, request
+from app import app, db, ma
 from tables import Customer, Orders
 
-class Customers_schema(ma.Schema):
+
+"""Определение полей схемы"""
+class CustomersSchema(ma.Schema):
     class Meta:
         fields = ('id', 'customer_name', 'phone_number')
-customers_schema_many = Customers_schema(many = True)
 
-# Роут для получения данных из таблицы customer по id, отсортированных по id, количество записей ограничено 15. Вид:
-# [
-#     {
-#         "customer_name": "Татьяна",
-#         "id": 1,
-#         "phone_number": "89002001020"
-#     }
-# ]
-@app.route('/customer/<id>', methods = ['GET'])
+"""Условность Marshmallow для получения нескольких запросов, таким образом данные будут попадать в список"""
+customers_schema_many = CustomersSchema(many=True)
+
+@app.route('/customer/<id>', methods=['GET'])
 def get_customers(id):
+    """Роут для получения данных из таблицы customer по id, отсортированных по id, количество записей ограничено 15. Вид:
+    [
+        {
+            "customer_name": "Татьяна",
+            "id": 1,
+            "phone_number": "89002001020"
+        }
+    ]"""
     get_customer = db.session.query(Customer).filter(Customer.id == id).order_by(Customer.id).limit(15)
     result = customers_schema_many.dump(get_customer)
+
     return jsonify(result)
 
-
-#Роут для получения всех записей из таблицы customer, отсортированных по id, количество записей ограничено 15
 @app.route('/customer', methods=['GET'])
 def get_customers_all():
+
+    """ Роут для получения всех записей из таблицы customer, отсортированных по id, количество записей ограничено 15. Вид:
+    [
+        {
+            "customer_name": "Татьяна",
+            "id": 1,
+            "phone_number": "89002001020"
+        }
+    ]"""
+
     all_customers = Customer.query.order_by(Customer.id).limit(15)
     result = customers_schema_many.dump(all_customers)
+
     return jsonify(result)
 
-#Роут для обновления записи в таблице customer по id, возвращает обновлённый список записей с сортировкой по id, количество записей ограничено 15.
-# #{
-#
-#         "customer_name": str,
-#         "phone_number" : str
-#
-# #}
-@app.route('/customer/<id>', methods = ['PATCH'])
+@app.route('/customer/<id>', methods=['PATCH'])
 def update_customers(id):
+    """#Роут для обновления записи в таблице customer по id, возвращает обновлённый список записей с сортировкой по id, количество записей ограничено 15.
+    Принимает данные вида:
+    {
 
+          "customer_name": str(not null),
+          "phone_number" : str
+
+    }"""
     id_req = id
     new_name = request.json['customer_name']
     new_number = request.json['phone_number']
@@ -50,10 +68,11 @@ def update_customers(id):
     result = customers_schema_many.dump(customer_)
     return jsonify(result)
 
-# Роут производит удаление заданного по id клиента из таблицы customer и связанные с ним по внешнему ключу записи из таблицы orders,
-# возвращает обновлённый список записей с сортировкой по id.
-@app.route('/customer/<id>', methods = ['DELETE'])
+@app.route('/customer/<id>', methods =['DELETE'])
 def del_customers(id):
+
+    """ Роут производит удаление заданного по id клиента из таблицы customer и связанные с ним по внешнему ключу записи из таблицы orders,
+        возвращает обновлённый список записей с сортировкой по id, количество записей ограничено 15."""
 
     id_req = id
     db.session.query(Orders).filter(Orders.id_customer == id_req).delete()
@@ -64,14 +83,15 @@ def del_customers(id):
     result = customers_schema_many.dump(customer_)
     return jsonify(result)
 
-# Роут выполняет добавление в таблицу customer новую запись, шаблон:
-# {
-# "customer_name": string
-# "phone_number": string
-# }
-# возвращает обновлённый список записей с сортировкой по id, количество записей ограничено 15
-@app.route('/customer', methods = ['PUT'])
+@app.route('/customer', methods=['PUT'])
 def new_customer():
+
+    """ Роут выполняет добавление в таблицу customer новую запись, шаблон:
+    {
+      "customer_name": string(not null)
+      "phone_number": string
+    }
+    возвращает обновлённый список записей с сортировкой по id, количество записей ограничено 15"""
 
     customer_name_req = request.json['customer_name']
     phone_number_req = request.json['phone_number']
--- orders_routes.py
+++ orders_routes.py
@@ -1,82 +1,97 @@
-from app import app, jsonify, db, request, ma
+"""Belialfff"""
+"""4 типа запросов(get,put,patch,delete) для таблицы orders"""
+
+
+from flask import jsonify, request
+from app import app, db, ma
 from tables import Orders
 
 
-class Orders_schema(ma.Schema):
+"""Определение полей схемы"""
+class OrdersSchema(ma.Schema):
     class Meta:
         fields = ('id', 'technic', 'price', 'id_customer')
-orders_schema_many = Orders_schema (many = True)
 
-#Роут для полчения записи из таблицы orders по id вида:
-# [
-#     {
-#         "id": 1,
-#         "id_customer": 1,
-#         "price": 1600,
-#         "technic": "Смартфон"
-#     }
-# ]
-@app.route('/orders/<id>', methods = ['GET'])
+"""Условность Marshmallow для получения нескольких запросов, таким образом данные будут попадать в список"""
+orders_schema_many = OrdersSchema(many=True)
+
+@app.route('/orders/<id>', methods=['GET'])
 def get_orders(id):
+
+    """Роут для полчения записи из таблицы orders по id вида:
+    [
+        {
+            "id": 1,
+            "id_customer": 1,
+            "price": 1600,
+            "technic": "Смартфон"
+        }
+    ] Сортировка по id , лимит записей 15"""
+
     get_order = db.session.query(Orders).filter(Orders.id == id).limit(15)
     result = orders_schema_many.dump(get_order)
+
     return jsonify(result)
 
-#Роут для полчения списка всех записей таблицы orders ,отсортированных по id, количество возвращаемых записей ограничено 15
-@app.route('/orders', methods = ['GET'])
+@app.route('/orders', methods=['GET'])
 def get_orders_all():
+
+    """ Роут для полчения списка всех записей таблицы orders ,отсортированных по id, количество возвращаемых записей ограничено 15."""
+
     all_orders = db.session.query(Orders).order_by(Orders.id).limit(15)
     result = orders_schema_many.dump(all_orders)
+
     return jsonify(result)
 
-#Роут для удаления записи из таблицы orders по id, возвращает все записи таблицы orders, отсортированные по id, количество возвращаемых записей ограничено 15
-@app.route('/orders/<id>', methods = ['DELETE'])
+@app.route('/orders/<id>', methods=['DELETE'])
 def del_orders(id):
 
-    id_req = id
+    """ Роут для удаления записи из таблицы orders по id, возвращает все записи таблицы orders, отсортированные по id,
+    количество возвращаемых записей ограничено 15"""
 
+    id_req = id
     db.session.query(Orders).filter(Orders.id == id_req).delete()
     db.session.commit()
-
     orders_ = db.session.query(Orders).order_by(Orders.id).limit(15)
     result = orders_schema_many.dump(orders_)
+
     return jsonify(result)
 
-# Роут выполняет добавление в таблицу orders новую запись, шаблон:
-# {
-# "id_customer" : int,
-# "technic": string,
-# "price": int
-# }
-# возвращает обновлённый список записей с сортировкой по id, количество записей ограничено 15
-@app.route('/orders', methods = ['PUT'])
+@app.route('/orders', methods=['PUT'])
 def new_orders():
+
+    """Роут выполняет добавление в таблицу orders новую запись, шаблон:
+    {
+      "id_customer" : int,
+      "technic": string,
+      "price": int
+     }
+    возвращает обновлённый список записей с сортировкой по id, количество записей ограничено 15"""
 
     technic_req = request.json['technic']
     price_req = request.json['price']
     id_customer_req = request.json['id_customer']
 
     new_order = Orders( technic_req, price_req, id_customer_req)
-
     db.session.add(new_order)
     db.session.commit()
-
     orders_ = db.session.query(Orders).all()
     result = orders_schema_many.dump(orders_)
 
     return jsonify(result)
 
-#Роут для обновления записи в таблице orders по id, возвращает обновлённый список записей с сортировкой по id, количество записей ограничено 15.
-#шаблон:
-# #{
-#
-#         "id_customer": int,
-#         "technic" : str,
-#         "price" : int
-#
-# #}
-@app.route('/orders/<id>', methods = ['PATCH'])
+@app.route('/orders/<id>', methods=['PATCH'])
 def update_orders(id):
+
+    """Роут для обновления записи в таблице orders по id, возвращает обновлённый список записей с сортировкой по id, количество записей ограничено 15.
+    шаблон:
+    {
+
+            "id_customer": int,
+            "technic" : str,
+            "price" : int
+
+    }"""
 
     id_req = id
     technic_req = request.json['technic']
@@ -85,7 +100,7 @@ def update_orders(id):
 
     db.session.query(Orders).filter(Orders.id == id_req).update(dict(technic = technic_req, price = price_req, id_customer = id_customer_req))
     db.session.commit()
-
     orders_ = db.session.query(Orders).order_by(Orders.id).limit(15)
     result = orders_schema_many.dump(orders_)
+
     return jsonify(result)
